14d135e7937e93f08dcc3ced2d2ab33d4d6038b4
Welder <lizakirienko007@gmail.com>
2024-04-12 15:00:10 +0200

    blender))

--- Code/Code/MainWindow.xaml.cs
+++ Code/Code/MainWindow.xaml.cs
@@ -5,10 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Media3D;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -23,6 +24,8 @@ namespace Code
         public MainWindow()
         {
             InitializeComponent();
+
+            
         }
     }
 }
